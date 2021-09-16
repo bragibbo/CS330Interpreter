1f4fa75cc3d5f50894594e692dacbab184bb5208
Brayden Gibbons <brayden.gibbons66@gmail.com>
2021-09-16 12:46:12 -0600

    switched to stdin for input

--- runner.js
+++ runner.js
@@ -3,14 +3,13 @@ const SExpressionParser = require("./s-expr-parser").SExpressionParser
 const PythonModuleParser = require("./python-module-parser").PythonModuleParser
 
 function main() {
-  // const stringInput = fs.readFileSync(0).toString();
-  const stringInput =
-    "(Module [body ((Expr [value (BinOp [left (Constant [value 5] [kind #f])] [op (Add)] [right (Constant [value 4] [kind #f])])]))] [type_ignores ()])";
+  const stringInput = fs.readFileSync(0).toString();
+  // const stringInput =
+  //   "(Module [body ((Expr [value (BinOp [left (Constant [value 5] [kind #f])] [op (Add)] [right (Constant [value 4] [kind #f])])]))] [type_ignores ()])";
 
-  let parsedString = SExpressionParser(stringInput, 0, []);
-  console.log("S-Expression Parsed\n" +JSON.stringify(parsedString, null, 2));
-  let ast = PythonModuleParser(parsedString)
-  console.log("Python Module Parsed\n" + JSON.stringify(ast, null, 1))
+  let parsedSExressionList = SExpressionParser(stringInput, 0, []);
+  let ast = PythonModuleParser(parsedSExressionList)
+  console.log("\n"+JSON.stringify(ast, null, 1))
 }
 
 main()
