bd3321b9a8c6fc032d308d7013662144ebedecf1
Brayden Gibbons <brayden.gibbons66@gmail.com>
2021-09-16 12:29:27 -0600

    first iteration of ast complete

--- python-module-parser.js
+++ python-module-parser.js
@@ -4,51 +4,140 @@ const {
   BinOp,
   ExprStamt,
   Module,
-  SExprParen,
+  Constant,
+  UnaryOperator,
+  AST,
 } = require("./types");
 
 module.exports.PythonModuleParser = (sExpressions) => {
-  return parseMod(sExpressions)
+  return new AST(parseMod(sExpressions));
 };
 
-function parseMod(listOfSExpr) {
-  return new Module(parseExprStmt(parseExprStmt()));
-}
-
-function parseExprStmt(listOfSExpr) {
-  return new ExprStamt();
-}
-
-function expr(listOfSExpr) {
-  switch (listOfSExpr[0]) {
-    case "BinOp":
-      return new BinOp(
-        expr(listOfSExpr[1].listOfSExpr),
-        operator(listOfSExpr[2].list)
-      );
-
-    case "UnaryOp":
-
-    case "Constant":
+function parseMod(sExpr) {
+  if (
+    sExpr.constructor.name === "SExprParen" &&
+    sExpr.listSExpr.length === 3 &&
+    sExpr.listSExpr[0].value === "Module"
+  ) {
+    let body = sExpr.listSExpr[1];
+    if (
+      body.constructor.name === "SExprBracket" &&
+      body.listSExpr.length === 2 &&
+      body.listSExpr[0].value === "body" &&
+      body.listSExpr[1].constructor.name === "SExprParen" &&
+      body.listSExpr[1].listSExpr.length === 1
+    ) {
+      return new Module(parseExprStmt(body.listSExpr[1].listSExpr[0]));
+    }
   }
+  throw new Error("Unable to Parse Module: " + sExpr);
 }
 
-function parseOperator(type) {
-  switch (type[0]) {
-    case "Add":
-      return new Operator(type);
-    case "Sub":
-      return new Operator(type);
-    case "Mult":
-      return new Operator(type);
+function parseExprStmt(sExpr) {
+  console.log(sExpr);
+  if (
+    sExpr.constructor.name === "SExprParen" &&
+    sExpr.listSExpr.length === 2 &&
+    sExpr.listSExpr[0].value === "Expr"
+  ) {
+    let expr = sExpr.listSExpr[1];
+    if (
+      expr.constructor.name === "SExprBracket" &&
+      expr.listSExpr.length === 2 &&
+      expr.listSExpr[0].value === "value"
+    ) {
+      return new ExprStamt(parseExpr(expr.listSExpr[1]));
+    }
   }
+  throw new Error("Unable to Parse ExprStmt: " + sExpr);
 }
 
-function parseUnaryOp(type) {
-  switch (type) {
-    case "UAdd":
-      return new UnaryOp(type);
-    case "USub":
-      return new UnaryOp(type);
+function parseExpr(sExpr) {
+  if (sExpr.constructor.name === "SExprParen" && sExpr.listSExpr.length > 0) {
+    switch (sExpr.listSExpr[0].value) {
+      case "BinOp":
+        if (
+          sExpr.listSExpr.length === 4 &&
+          sExpr.listSExpr[1].constructor.name === "SExprBracket" &&
+          sExpr.listSExpr[2].constructor.name === "SExprBracket" &&
+          sExpr.listSExpr[3].constructor.name === "SExprBracket" &&
+          sExpr.listSExpr[1].listSExpr.length === 2 &&
+          sExpr.listSExpr[1].listSExpr[0].value === "left" &&
+          sExpr.listSExpr[2].listSExpr.length === 2 &&
+          sExpr.listSExpr[1].listSExpr[0].constructor.name === "Atom" &&
+          sExpr.listSExpr[2].listSExpr[0].value === "op" &&
+          sExpr.listSExpr[3].listSExpr.length === 2 &&
+          sExpr.listSExpr[3].listSExpr[0].value === "right"
+        ) {
+          return new BinOp(
+            parseExpr(sExpr.listSExpr[1].listSExpr[1]),
+            parseOperator(sExpr.listSExpr[2].listSExpr[1]),
+            parseExpr(sExpr.listSExpr[3].listSExpr[1])
+          );
+        }
+        break;
+
+      case "UnaryOp":
+        if (
+          sExpr.listSExpr.length === 3 &&
+          sExpr.listSExpr[1].constructor.name === "SExprBracket" &&
+          sExpr.listSExpr[2].constructor.name === "SExprBracket" &&
+          sExpr.listSExpr[1].listSExpr.length === 2 &&
+          sExpr.listSExpr[1].listSExpr[0].constructor.name === "Atom" &&
+          sExpr.listSExpr[1].listSExpr[0].value === "op" &&
+          sExpr.listSExpr[2].listSExpr.length === 2 &&
+          sExpr.listSExpr[2].listSExpr[0].value === "operand"
+        ) {
+          return new UnaryOp(
+            parseUnaryOp(sExpr.listSExpr[1].listSExpr[1]),
+            parseExpr(sExpr.listSExpr[2].listSExpr[1])
+          );
+        }
+        break;
+
+      case "Constant":
+        if (
+          sExpr.listSExpr.length === 3 &&
+          sExpr.listSExpr[1].constructor.name === "SExprBracket" &&
+          sExpr.listSExpr[1].listSExpr.length === 2 &&
+          sExpr.listSExpr[1].listSExpr[0].constructor.name === "Atom" &&
+          sExpr.listSExpr[1].listSExpr[0].value === "value" &&
+          sExpr.listSExpr[2].listSExpr[0].constructor.name === "Atom" &&
+          sExpr.listSExpr[2].listSExpr[0].value === "kind"
+        ) {
+          return new Constant(sExpr.listSExpr[1].listSExpr[1].value, sExpr.listSExpr[2].listSExpr[1].value);
+        }
+        break;
+    }
   }
+
+  throw new Error("Unable to parse Expr: " + sExpr);
+}
+
+function parseOperator(sExpr) {
+  if (sExpr.listSExpr.length === 1 && sExpr.listSExpr[0].constructor.name === "Atom") { 
+    switch (sExpr.listSExpr[0].value) {
+      case "Add":
+        return new Operator(sExpr.listSExpr[0].value);
+      case "Sub":
+        return new Operator(sExpr.listSExpr[0].value);
+      case "Mult":
+        return new Operator(sExpr.listSExpr[0].value);
+    }
+  }
+
+  throw new Error("Unable to parse Op: " + sExpr)
+}
+
+function parseUnaryOp(sExpr) {
+  if (sExpr.listSExpr.length === 1 && sExpr.listSExpr[0].constructor.name === "Atom") {
+    switch (sExpr.listSExpr[0].value) {
+      case "UAdd":
+        return new UnaryOperator(type);
+      case "USub":
+        return new UnaryOperator(type);
+    }
+  }
+
+  throw new Error("Unable to parse UnaryOp: " + sExpr)
 }
--- types.js
+++ types.js
@@ -9,7 +9,7 @@ class SExprParse {
     }
   }
 }
-exports.SExprParse = SExprParse
+exports.SExprParse = SExprParse;
 
 class SExprBracket {
   constructor(sexpr) {
@@ -20,7 +20,7 @@ class SExprBracket {
     }
   }
 }
-exports.SExprBracket = SExprBracket
+exports.SExprBracket = SExprBracket;
 
 class SExprParen {
   constructor(sexpr) {
@@ -31,49 +31,72 @@ class SExprParen {
     }
   }
 }
-exports.SExprParen = SExprParen
+exports.SExprParen = SExprParen;
 
 class Atom {
   constructor(type, value) {
-    this.type = type
-    this.value = value
+    this.type = type;
+    this.value = value;
   }
 }
-exports.Atom = Atom
+exports.Atom = Atom;
+
+class AST {
+  constructor(module) {
+    this.module = module;
+  }
+}
+exports.AST = AST;
 
 class Module {
   constructor(exprStmt) {
-    this.exprStmt = exprStmt
+    this.exprStmt = exprStmt;
   }
 }
-exports.Module = Module
+exports.Module = Module;
 
 class ExprStamt {
   constructor(expr) {
-    this.expr = expr
+    this.expr = expr;
   }
 }
-exports.ExprStamt = ExprStamt
+exports.ExprStamt = ExprStamt;
 
 class BinOp {
   constructor(left, op, right) {
-    this.left = left
-    this.op = op
-    this.right = right
+    this.left = left;
+    this.op = op;
+    this.right = right;
   }
 }
-exports.BinOp = BinOp
+exports.BinOp = BinOp;
+
+class UnaryOp {
+  constructor(op, operand) {
+    this.op = op;
+    this.operand = operand;
+  }
+}
+exports.UnaryOp = UnaryOp;
+
+class Constant {
+  constructor(value, kind) {
+    this.value = value;
+    this.kind = kind;
+  }
+}
+exports.Constant = Constant;
 
 class Operator {
   constructor(type) {
     this.type = type;
   }
 }
-exports.Operator = Operator
+exports.Operator = Operator;
 
-class UnaryOp {
+class UnaryOperator {
   constructor(type) {
     this.type = type;
   }
 }
-exports.UnaryOp = UnaryOp
+exports.UnaryOperator = UnaryOperator;
